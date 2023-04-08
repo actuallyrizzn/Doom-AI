d63a263121fd52f9a15d149880ea6f29d1f3b7bd
Jordan Watson <jordanwatson915@gmail.com>
2023-04-08 10:50:55 -0700

    Implement count neurons function

--- ai.py
+++ ai.py
@@ -23,7 +23,9 @@ import experience_replay, image_preprocessing
 class CNN(nn.Module):
 
     def __init__(self, number_actions):
-        """ CNN with 3 convolutional layers and 1 hidden layer """
+        """ 
+        CNN with 3 convolutional layers and 1 hidden layer 
+        """
         
         super(CNN, self).__init__() # Activate inheritance to use tools from nn.Module
         # Convolution connections
@@ -32,12 +34,24 @@ class CNN(nn.Module):
         self.convolution3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2) 
         # Flatten pixels obtained by the convolutions that were applied to get a vector
         # Vector that will be used as the input for the NN
-        self.fc1 = nn.Linear(in_features=self.count_neurons(), out_features=40) # Full connection between input layer (vector) and hidden layer
+        self.fc1 = nn.Linear(in_features=self.count_neurons(image_dim=(1, 80, 80)), out_features=40) # Full connection between input layer (vector) and hidden layer
         self.fc2 = nn.Linear(in_features=40, out_features=number_actions) # Full connection between the hidden layer and output layer composed on the output neurons that correspond to a Q value of the possible actions
         
-    def count_neurons(self):
-        """ Count the number of pixels """
-        return 0
+    def count_neurons(self, *image_dim):
+        """ 
+        Count the number of pixels, which represent the number of neurons
+        in the vectpr after the convolutions are applied 
+        """
+        kernal_size = 3
+        stride = 2
+        x = Variable(torch.rand(1, *image_dim)) # Input image
+        # Propagate image into the NN to reach the flattening layer to get the number of neurons we want
+        x = F.relu(F.max_pool2d(self.convolution1(x), kernal_size, stride)) # First convolutional layer with max pooling and ReLU activation
+        x = F.relu(F.max_pool2d(self.convolution2(x), kernal_size, stride)) # Propagate images from first to second convolutional layer
+        x = F.relu(F.max_pool2d(self.convolution3(x), kernal_size, stride)) # Propagate images from second to third convolutional layer
+        # Flatten pixels of third convolutional layer
+        # Flattening layer
+        return x.data.view(1, -1).size(1) # Takes all pixels of all the third layer channels and puts them in a vector (input of the fully connected network)
 
 # Making the body
 
